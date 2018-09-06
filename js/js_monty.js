0d62d5edaf7e724d3b3005584f5b1e96bf78cbc7
montague michael llewelyn morphy <845873736@cit3-501m-14.acg.ac.nz>
2018-09-06 15:12:28 +1200

    added some icons

--- js/js_monty.js
+++ js/js_monty.js
@@ -7,7 +7,42 @@ const key = version + clientId + clientSecret;
 console.log(key)
 var map;
 var venueGroup;
+var customRadius;
 
+
+var locationIcon = L.icon({
+	iconUrl:'custom_images/icon_location.svg',
+	iconSize: [40,40],
+});
+
+var barIcon = L.icon({
+	iconUrl:'custom_images/icon_bar.svg',
+	iconSize: [40,40],
+});
+
+var foodIcon = L.icon({
+	iconUrl:'custom_images/icon_knifefork.svg',
+	iconSize: [40,40],
+});
+var parkIcon = L.icon({
+	iconUrl:'custom_images/icon_park.svg',
+	iconSize: [40,40],
+});
+
+var hotelIcon = L.icon({
+	iconUrl:'custom_images/icon_hotel.svg',
+	iconSize: [40,40],
+});
+
+var gymIcon = L.icon({
+	iconUrl:'custom_images/icon_gym.svg',
+	iconSize: [40,40],
+});
+
+var cafeIcon = L.icon({
+	iconUrl:'custom_images/icon_coffee.svg',
+	iconSize: [40,40],
+});
 
 
 $(function(){
@@ -21,16 +56,21 @@ $(function(){
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhhbHl4OTAiLCJhIjoiY2o2YjdrZHRlMWJmYjJybDd2cW1rYnVnNSJ9.j_DQLfixHfhioVjH6qmqkw').addTo(map);
 
-
+	defaultRadius = $('.slider').val();
 	var circle = L.circle(center,{
-		radius:300,
+		radius:defaultRadius,
 		weight:2,
 		className:'circle'
 	}).addTo(map);
-	console.log(circle)
-	console.log(circle._radius)
+	customRadius = circle._mRadius
+	
 
-	$( '#map').dblclick(function(e) {
+
+
+
+
+
+	$('#map').dblclick(function(e) {
 	  	var center = map.mouseEventToLatLng(e.originalEvent);
 
   		circle.setLatLng(center)
@@ -38,6 +78,15 @@ $(function(){
 	});
 
 	//End of Leaflet Map
+
+	//Circle slider
+	$('.slider').on('change',function(){
+			customRadius = circle.setRadius($('.slider').val());;
+			
+			customRadius = customRadius._mRadius
+
+
+		})
 
 
 
@@ -49,7 +98,7 @@ $(function(){
 //Find venues in selected area
 function loadVenues(lat,lng){
 
-	let exploreUrl = 'https://api.foursquare.com/v2/venues/explore'+key+'&radius=300&ll='+lat+','+lng;
+	let exploreUrl = 'https://api.foursquare.com/v2/venues/explore'+key+'&limit=50&radius='+customRadius+'&ll='+lat+','+lng;
 
 	$.ajax({
 		url:exploreUrl,
@@ -62,11 +111,33 @@ function loadVenues(lat,lng){
 					latlng:{lat:item.venue.location.lat,lng:item.venue.location.lng},
 					name:item.venue.name,
 					venueid:item.venue.id,
+					category:item.venue.categories["0"].name
 				};
 			});
 			venueGroup.clearLayers();
 			_(venues).each(function(venue){
-				let marker = L.marker(venue.latlng).addTo(venueGroup);
+
+				switch(venue.category) {
+				    case "Park":
+				        icon = parkIcon;
+				        break;
+				    case "Hotel":
+				        icon = hotelIcon;
+				        break;
+				    case "Gym":
+				        icon = gymIcon;
+				        break;
+				    case "Bar":
+				        icon = barIcon;
+				        break;
+				    case "Coffee Shop", "Café":
+				        icon = cafeIcon;
+				        break;
+				    default:
+				        icon = locationIcon
+				}
+
+				let marker = L.marker(venue.latlng,{icon:icon}).addTo(venueGroup);
 				marker.bindPopup('<div>'+venue.name+'</div>');
 			});
 		}
