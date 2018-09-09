9b8d8fd025c043e7351e0c2db83041367480832c
Creeshana jade Robati-kongaika <grish@Creeshanas-iMac.local>
2018-09-10 10:34:09 +1200

    Finished navigation

---
+++ js/js_cree.js
@@ -0,0 +1,24 @@
+    // Open and close nav on mobile
+    $('.bars').on('click', function(e) {
+
+        var navData = $('.navigation').data('nav');
+
+        e.stopPropagation();
+
+        if (navData == 'close') {
+            $('.navigation').addClass('nav-open')
+                .data('nav', 'open')
+
+            $('.bars>i').first().removeClass('fas fa-bars')
+                .addClass('fas fa-times');
+
+            $('.heading,.sub-heading').addClass('text-hide');
+        } else {
+            $('.navigation').removeClass('nav-open')
+                .data('nav', 'close');
+            $('.bars>i').removeClass('fas fa-times')
+                .addClass('fas fa-bars');
+
+            $('.heading,.sub-heading').removeClass('text-hide');
+        }
+    });
