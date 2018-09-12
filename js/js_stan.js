829454e9e5f2bf4cf6cda600ad510ce616c5bd62
ngorn leng khun <845842165@cit3-501m-20.acg.ac.nz>
2018-09-12 15:15:37 +1200

    working on buttons

--- js/js_stan.js
+++ js/js_stan.js
@@ -1,6 +1,8 @@
 $(function(){
 
 	// button animation
+
+	// FOOD
 	// -------------------------------------------
 	var oCafeAnime= anime({
 		targets:'.cafe_wrap',
@@ -9,10 +11,7 @@ $(function(){
 		easing:'linear',
 		duration:900,
 		autoplay:true,
-		complete:function(){
-			$('.cafe_circle').css('animation','draw 2s linear');
-			$('.inner_cafe_circle').css('animation','reverse_draw 2s linear');
-		}
+	
 	});
 
 	var oBarAnime= anime({
@@ -22,10 +21,7 @@ $(function(){
 		easing:'linear',
 		duration:1100,
 		autoplay:true,
-		complete:function(){
-			$('.bar_circle').css('animation','draw 2s linear');
-			$('.inner_bar_circle').css('animation','reverse_draw 2s linear');
-		}
+		
 	});
 
 	var oRestaurantAnime= anime({
@@ -35,30 +31,101 @@ $(function(){
 		easing:'linear',
 		duration:1300,
 		autoplay:true,
-		complete:function(){
-			$('.restaurant_circle').css('animation','draw 2s linear');
-			$('.inner_restaurant_circle').css('animation','reverse_draw 2s linear');
-		}
+		
 	});
 
-
-
-
-	var oAllAnime= anime({
-		targets:'.all_wrap',
+	var oFoodAllAnime= anime({
+		targets:'.food_all_wrap',
 		translateX:[-100,0],
 		opacity:[0,1],
 		easing:'linear',
 		duration:1500,
 		autoplay:true,
 		complete:function(){
-			$('.all_circle').css('animation','draw 2s linear');
-			$('.inner_all_circle').css('animation','reverse_draw 2s linear');
+			$('.food_all_circle').css('animation','draw 2s linear');
+			$('.inner_food_all_circle').css('animation','reverse_draw 2s linear');
+			$('.food_all_circle').css('stroke','#79E8CC');
+			$('.inner_food_all_circle').css('stroke','#79E8CC');
+
+			$('.restaurant_circle').css('animation','draw 2s linear');
+			$('.inner_restaurant_circle').css('animation','reverse_draw 2s linear');
+			$('.restaurant_circle').css('stroke','#79E8CC');
+			$('.inner_restaurant_circle').css('stroke','#79E8CC');
+
+			$('.bar_circle').css('animation','draw 2s linear');
+			$('.inner_bar_circle').css('animation','reverse_draw 2s linear');
+			$('.bar_circle').css('stroke','#79E8CC');
+			$('.inner_bar_circle').css('stroke','#79E8CC');
+
+			$('.cafe_circle').css('animation','draw 2s linear');
+			$('.inner_cafe_circle').css('animation','reverse_draw 2s linear');
+			$('.cafe_circle').css('stroke','#79E8CC');
+			$('.inner_cafe_circle').css('stroke','#79E8CC');
 		}
 	});
 
+	// ACCOMMODATION
+	// -------------------------------------------
 
+	var oHotelAnime= anime({
+		targets:'.hotel_wrap',
+		translateX:[-100,0],
+		opacity:[0,1],
+		easing:'linear',
+		duration:900,
+		autoplay:true,
+	
+	});
 
-	// console.log(oRestaurantAnime)
+	var oMotelAnime= anime({
+		targets:'.motel_wrap',
+		translateX:[-100,0],
+		opacity:[0,1],
+		easing:'linear',
+		duration:1100,
+		autoplay:true,
+		
+	});
+
+	var oBackpackAnime= anime({
+		targets:'.backpack_wrap',
+		translateX:[-100,0],
+		opacity:[0,1],
+		easing:'linear',
+		duration:1300,
+		autoplay:true,
+		
+	});
+
+	var oAccomAllAnime= anime({
+		targets:'.accom_all_wrap',
+		translateX:[-100,0],
+		opacity:[0,1],
+		easing:'linear',
+		duration:1500,
+		autoplay:true,
+		complete:function(){
+			$('.accom_all_circle').css('animation','draw 2s linear');
+			$('.inner_accom_all_circle').css('animation','reverse_draw 2s linear');
+			$('.accom_all_circle').css('stroke','#79E8CC');
+			$('.inner_accom_all_circle').css('stroke','#79E8CC');
+
+			$('.hotel_circle').css('animation','draw 2s linear');
+			$('.inner_hotel_circle').css('animation','reverse_draw 2s linear');
+			$('.hotel_circle').css('stroke','#79E8CC');
+			$('.inner_hotel_circle').css('stroke','#79E8CC');
+
+			$('.motel_circle').css('animation','draw 2s linear');
+			$('.inner_motel_circle').css('animation','reverse_draw 2s linear');
+			$('.motel_circle').css('stroke','#79E8CC');
+			$('.inner_motel_circle').css('stroke','#79E8CC');
+
+			$('.backpack_circle').css('animation','draw 2s linear');
+			$('.inner_backpack_circle').css('animation','reverse_draw 2s linear');
+			$('.backpack_circle').css('stroke','#79E8CC');
+			$('.inner_backpack_circle').css('stroke','#79E8CC');
+		}
+	});
+
 
 });
