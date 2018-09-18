db78bf89ef9c4cd8c1b9ae740d00d6d3204fceb4
Creeshana jade Robati-kongaika <866131226@cit3-501m-12.acg.ac.nz>
2018-09-18 15:41:20 +1200

    merging with monty_branch

--- js/more-details-test.js
+++ js/more-details-test.js
@@ -1,6 +1,6 @@
 const version = '?v=20170901';
-const clientId = '&client_id=TEOMTULWIDZGIMDRL31PQEQAHFI550SUFDSK3S42ZUTLU0PS';
-const clientSecret = '&client_secret=JDNFWZICWDDLBE43XDPEA4BWG3F0GNXIVCWVDQQF534UN5DL';
+const clientId = '&client_id=NRDFRIMIWO5QY4H3HTPITKL3JKOJLBJTWUHBUYJE5JHB5XCB';
+const clientSecret = '&client_secret=GIFUXB102BZZ0KXFTOCZ5XGWN1T5LLZG1NU0G5JKFRVP55DN';
 
 const key = version + clientId + clientSecret;
 
@@ -348,10 +348,6 @@ $(function(){
 //------TATIANA's code finished here
 
 
-//------Cree's javascript - More details
-
-
-//------Cree's javascript - More details E N D
 	
 
 });
@@ -380,170 +376,181 @@ function loadVenues(lat,lng){
 					address:item.venue.location.formattedAddress,
 
 				};
-
-
 			});
-
-		
-
 
 			venueGroup.clearLayers();
 			_(venues).each(function(venue){
 
 				switch(venue.category) {
-					case "Park":
-					icon = parkIcon;
-					break;
-					case "Scenic Lookout":
-					icon = parkIcon;
-					break;
-					case "Hotel":
-					icon = hotelIcon;
-					break;
-					case "Gym":
-					case "Gym / Fitness Center":
-					icon = gymIcon;
-					break;
-					case "Bar":
-					case "Cocktail Bar": 
-					icon = barIcon;
-					break;
-					case "Coffee Shop":
-					case "Café":
-					case "Coffee":
-					case "Bistro":
-					icon = cafeIcon;
-					break;
-					case "Sandwich Place":
-					case "Muffin Break":
-					case "Dessert Shop":
-					case "Snack Place":
-					case "Restaurant":
-					case "Pub":
-					case "Pizza":
-					case "Burger Joint":
-					case "Pizza Place":
-					case "Restaurant":
-					case "Asian Restaurant":
-					case "Seafood Restaurant":
-					case "Korean Restaurant":
-					case "Thai Restaurant":
-					case "Portuguese Restaurant":
-					case "Japanese Restaurant":
-					case "Chinese Restaurant":
-					case "Indian Restaurant":
-					case "Italian Restaurant":
-					case "Fast Food Restaurant":
-					case "Sushi Restaurant":
-					case "Turkish Restaurant":
-					case "Mediterranean Restaurant":
-					case "Falafel Restaurant":
-					case "Mexican Restaurant":
-					case "Bakery":
-					case "Grocery Store":
-					case "Supermarket":
-					case "Noodle House":
-					case "Ice Cream Shop":
-					case "BBQ Joint":
-					case "Fish & Chips Shop":
-					case "Steakhouse":
-					case "Liquor Store":
-					icon = foodIcon;
-					break;
-					case "Harbor / Marina":
-					icon = harbourIcon
-					break;
-					case "Trail":
-					icon = trailIcon
-					break;
-					case "Bus Station":
-					icon = busIcon
-					break;
-					case "Gas Station":
-					icon = gasIcon
-					break;
-					case "Liquor Store":
-					icon = parkIcon
-					break;
-					default:
-					icon = locationIcon
+				    case "Park":
+				        icon = parkIcon;
+				        break;
+				    case "Scenic Lookout":
+				        icon = parkIcon;
+				        break;
+				    case "Hotel":
+				        icon = hotelIcon;
+				        break;
+				    case "Gym":
+				    case "Gym / Fitness Center":
+				        icon = gymIcon;
+				        break;
+				    case "Bar":
+				    case "Cocktail Bar": 
+				        icon = barIcon;
+				        break;
+				    case "Coffee Shop":
+				    case "Café":
+				    case "Coffee":
+				    case "Bistro":
+				        icon = cafeIcon;
+				        break;
+				    case "Sandwich Place":
+				    case "Muffin Break":
+				    case "Dessert Shop":
+				    case "Snack Place":
+				    case "Restaurant":
+				    case "Pub":
+				    case "Pizza":
+				    case "Burger Joint":
+				    case "Pizza Place":
+				    case "Restaurant":
+				    case "Asian Restaurant":
+				    case "Seafood Restaurant":
+				    case "Korean Restaurant":
+				    case "Thai Restaurant":
+				    case "Portuguese Restaurant":
+				    case "Japanese Restaurant":
+				    case "Chinese Restaurant":
+				    case "Indian Restaurant":
+				    case "Italian Restaurant":
+				    case "Fast Food Restaurant":
+				    case "Sushi Restaurant":
+				    case "Turkish Restaurant":
+				    case "Mediterranean Restaurant":
+				    case "Falafel Restaurant":
+				    case "Mexican Restaurant":
+				    case "Bakery":
+				    case "Grocery Store":
+				    case "Supermarket":
+				    case "Noodle House":
+				    case "Ice Cream Shop":
+				    case "BBQ Joint":
+				    case "Fish & Chips Shop":
+				    case "Steakhouse":
+				    case "Liquor Store":
+				    	icon = foodIcon;
+				    	break;
+				   	case "Harbor / Marina":
+				   		icon = harbourIcon
+				   		break;
+				   	case "Trail":
+				   		icon = trailIcon
+				   		break;
+				   	case "Bus Station":
+				   		icon = busIcon
+				   		break;
+				   	case "Gas Station":
+				   		icon = gasIcon
+				   		break;
+				   	case "Liquor Store":
+				   		icon = parkIcon
+				   		break;
+				    default:
+				        icon = locationIcon
 				}
 
 
 				var myIcon = L.divIcon({ 
-					iconSize: new L.Point(35, 35), 
-					html: '<div class="custom-icon"><img class="the-icon" src="'+icon+'"></div>'
+				    iconSize: new L.Point(35, 35), 
+				    html: '<div class="custom-icon"><img class="the-icon" src="'+icon+'"></div>'
 				});
 
 				let marker = L.marker(venue.latlng,{icon:myIcon}).addTo(venueGroup);
 				marker.bindPopup('<div class="custom-map-popup"><img src="'+icon+'"><h1>'+venue.name+'</h1><p>"'+venue.address+'"</p><a href="#" class="more-info sqr-bttn btn btn-primary"  data-toggle="modal" data-target="#exampleModalLong">More info</a><a href="#" class="get-directions sqr-bttn btn btn-primary">Get directions</a></div>');
 
+				
+
+				// - - * * Cree's Script - More Details - - * *
+
 				var venueInfo = $('.more-info');
 
 				marker.venueid = venue.venueid;
 
-				marker.on('click',function(){
+				marker.on('click', function(){
 
 					var venueUrl = 	'https://api.foursquare.com/v2/venues/'+
 					this.venueid+key;
 
-					console.log(venueUrl);
+					// console.log(venueUrl);
 
 					$.ajax({
 						url:venueUrl,
 						dataType:'jsonp',
 						success:function(res){ 
+
 							console.log(res);
+
 							var venue = res.response.venue;
+
 							$('.modal-title').text(venue.name);
-							// $('.venue-description').text(venue.page.pageInfo.description);
+							$('.venue-description').text(venue.description);
 							$('.venue-location span').text(venue.location.formattedAddress);
 							$('.venue-phone-number span').text(venue.contact.formattedPhone);
-							$('.venue-open-hours span.open-now').text(venue.contact.richStatus.status);
-							$('.venue-open-hours ul.weekly-hours li').text(venue.contact.richStatus.status);
+							$('.venue-price-range span').text(venue.price.message);
 
-							
 						}
-					});
 
-					// Venue opening hours
+					});		// * * * ajax * * => 		E N D
 
 					var openHours = $('.venue-open-hours');
-
+					
 					openHours.on('click', function(){
+
 
 
 						var weeklyHours = $('.venue-open-hours').data('reveal');
 
-						// Open and close opening hours
+						// * * * Open and close .weekly-hours when .venue-open-hours is clicked
 
 						if (weeklyHours == 'close'){
-							console.log(weeklyHours);
 
 							$('.venue-open-hours ul.weekly-hours').addClass('open-hours')
 							$('.venue-open-hours').data('reveal', 'open');
 
 						} else {
-							console.log(weeklyHours);
+
 							$('.venue-open-hours ul.weekly-hours').removeClass('open-hours').data('reveal', 'close');
 							$('.venue-open-hours').data('reveal', 'close');
 
-						}
 
+						}		// * * * weeklyHours * * => 		if else E N D
 
-       					// Get Venue Hours 
+       					// * * * Request Venue Hours 
 
-       					marker.venueid = venue.venueid;
+       // 					venueid = venue.venueid;
+       					
+       // 					let hoursUrl = 'https://api.foursquare.com/v2/venues/'+venueid+'/hours'+key;
 
-       					let hoursUrl = 'https://api.foursquare.com/v2/venues/'+venueid+'/hours'+key;
+       // 					$.ajax({
 
-       					console.log(hoursUrl);
+       // 						url:hoursUrl,
+							// dataType:'jsonp',
+							// success:function(res){
+								
+								
+							// }
 
+       // 					});		// * * * ajax * * => 		E N D
 
-       				});
- 
-				});
-			}
+					}); 	// * * * openHours * * => 		on click event E N D
+
+				}); 	// * * * marker * *  => 		on click event E N D
+
+				// - - * * Cree's Script - - - - - E N D - - * *
+
+			});
+		}
 
 	});
 }
