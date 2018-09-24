937961727b160d9531e85b8c8fa9396ead7b6aac
montague michael llewelyn morphy <845873736@cit3-501m-14.acg.ac.nz>
2018-09-24 13:04:51 +1200

    presentable product,loginsignup fixed

---
+++ js/cube.js
@@ -0,0 +1,60 @@
+    
+// Monty Cube
+$(function(){
+var cube = $('.cube')
+var left = $('.button-left')
+var right = $('.button-right')
+var top = $('.button-top')
+var bottom = $('.button-bottom')
+var random = $('.button-random')
+
+
+
+        random.on('click',function(){
+            // cube.removeClass('rotate-left rotate-top')
+            // cube.addClass('random')
+            $('.cube').css('transform' , "rotate3d("+Math.floor((Math.random() * 360) + 1)+","+Math.floor((Math.random() * 360) + 1)+","+Math.floor((Math.random() * 360) + 1)+","+Math.floor((Math.random() * 360) + 1)+"deg)")
+        });
+
+        left.on('click',function(){
+            $('.cube').css('transform','rotate3d(-20,100,10,-100deg)')
+        });
+
+        right.on('click',function(){
+            $('.cube').css('transform','rotate3d(0,100,10,100deg)')
+        });
+
+
+        top.on('click',function(){
+         
+        });
+
+
+//Arrow controls
+var xAngle = 0, yAngle = 0;
+document.addEventListener('keydown', function(e) {
+  switch(e.keyCode) {
+
+    case 37: // left
+      yAngle -= 92;
+      break;
+
+    case 38: // up
+      xAngle += 92;
+      break;
+
+    case 39: // right
+      yAngle += 92;
+      break;
+
+    case 40: // down
+      xAngle -= 92;
+      break;
+  };
+
+  $('.cube').css('transform' , "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)");
+}, false);
+
+});
+
+
